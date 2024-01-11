597e5ed37384b632e813f9642ea6863f07c57f2f
clairemuiru1 <muiruclaire@gmail.com>
2024-01-11 10:36:49 +0300

    "Done with assingment"

--- server/app.py
+++ server/app.py
@@ -19,15 +19,41 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal =Animal.query.filter(Animal.id == id).first()
+    response_body = f''
+    response_body += f'<ul>ID: {animal.id}</ul>'
+    response_body += f'<Ul>Name: {animal.name}</ul>'
+    response_body += f'<ul>Species: {animal.species}</li>'
+    response_body += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
+    response_body += f'<ul>Enclosure : {animal.enclosure.environment}</ul>'
+
+    return make_response(response_body)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    resonse_body = f''
+    response_body += f'<ul> ID:{zookeeper.id}</ul>'
+    respnse_body += f'<ul> Name:{zookeeper.name}</ul>'
+    response__body += f'<ul>Birthday:{zookeeper.birthday}<ul>'
+
+    for animal in zookeeper.animals:
+        response_body += f'<ul>{animal.name}</ul>'
+
+    return make_response(resonse_body)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = enclosure.query.filter(enclosure.id == id).first()
+    response_body =f''
+    response_body += f'<ul> ID:{enclosure.id}</ul>'
+    response_body += f'<ul>Environment:{enclosure.environment}<ul>'
+    response_body += f'<ul>Open to visitors:{enclosure.open_to_visitors}<ul>'
+
+    for animal in enclosure.animals:
+        response_body += f'<ul>Animal:{animal.name}<ul>'
+    
+    return make_response (response_body)
 
 
 if __name__ == '__main__':
